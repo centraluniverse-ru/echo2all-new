fd7c2e4f75a05b54be58265fb7cf41bfd44d347c
centraluniverse <box@centraluniverse.ru>
2025-03-29 16:28:30 +0300

    feat(captcha)

--- app/actions.ts
+++ app/actions.ts
@@ -1,119 +1,104 @@
 "use server";
-import { Telegraf } from "telegraf";
-import {
-  InputMediaDocument,
-  InputMediaPhoto,
-  InputMediaVideo,
-} from "telegraf/types";
+import {Telegraf} from "telegraf";
+import {InputMediaDocument, InputMediaPhoto, InputMediaVideo,} from "telegraf/types";
 
-export async function sendMessageToTelegram(_: unknown, form?: FormData) {
-  if (!form) {
-    return { ok: false, message: "Форма не передана на бэкенд." };
-  }
+export async function sendMessageToTelegram(_: unknown, form: FormData, token: unknown) {
+    try {
+        const response = await fetch('https://www.google.com/recaptcha/api/siteverify', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            body: `secret=${process.env.RECAPTCHA_SECRET_KEY}&response=${token}`,
+        });
+        if (!response.ok) return {ok: false, message: "Ошибка при верификации качи"}
+        const data = await response.json()
 
-  let message = form.get("message")?.toString();
-  const files = form.getAll("files") as unknown as File[];
+        if (!data.success || data.score <= 0.5) return {ok: false, message: "Не пройдена проверка капчи"}
 
-  if (!message && !files)
-    return {
-      ok: false,
-      message: "Сообщение пустое или произошла внутренняя ошибка",
+    } catch {
+        return {ok: false, message: "Ошибка при верификации качи"}
+    }
+
+    if (!form) {
+        return {ok: false, message: "Форма не передана на бэкенд."};
+    }
+
+    let message = form.get("message")?.toString();
+    const files = form.getAll("files") as unknown as File[];
+    if ((!message || message == "") && (files.length == 1 && files[0].size == 0)) return {
+        ok: false, message: "Сообщение пустое.",
     };
 
-  const BOT_TOKEN = process.env.BOT_TOKEN;
-  const CHAT_ID = parseInt(process.env.CHAT_ID ?? "0");
+    const BOT_TOKEN = process.env.BOT_TOKEN;
+    const CHAT_ID = parseInt(process.env.CHAT_ID ?? "0");
 
-  if (!BOT_TOKEN || !CHAT_ID) {
-    return { ok: false, message: "Произошла внутренняя ошибка" };
-  }
-
-  let reply_id = undefined;
-
-  if (message) {
-    if (message.startsWith("cue2a_")) {
-      try {
-        reply_id = parseInt(message.split(" ", 1)[0].replace("cue2a_", ""));
-        message = message.replace(`cue2a_${reply_id}`, "");
-      } catch (e) {
-        console.log("Error while parsing reply_id");
-        console.error(e);
-      }
+    if (!BOT_TOKEN || !CHAT_ID) {
+        return {ok: false, message: "Произошла внутренняя ошибка"};
     }
-  }
 
-  const tg = new Telegraf(BOT_TOKEN);
+    let reply_id = undefined;
 
-  if (files[0].size > 0) {
-    const mediagroup = [];
-
-    const allImagesOrVideos = files.every(
-      (file) => file.type.startsWith("image/") || file.type.startsWith("video/")
-    );
-    const allAudios = files.every((file) => file.type.startsWith("audio/"));
-
-    if (allImagesOrVideos) {
-      for (const file of files) {
-        mediagroup.push({
-          type: file.type.split("/")[0] == "image" ? "photo" : "video",
-          media: {
-            source: Buffer.from(await file.arrayBuffer()),
-            filename: file.name,
-          },
-          caption: message,
-        });
-      }
-    } else if (allAudios) {
-      for (const file of files) {
-        mediagroup.push({
-          type: file.type.split("/")[0],
-          media: {
-            source: Buffer.from(await file.arrayBuffer()),
-            filename: file.name,
-          },
-          caption: message,
-        });
-      }
-    } else {
-      for (const file of files) {
-        mediagroup.push({
-          type: "document",
-          media: {
-            source: Buffer.from(await file.arrayBuffer()),
-            filename: file.name,
-          },
-          caption: message,
-        });
-      }
+    if (message) {
+        if (message.startsWith("cue2a_")) {
+            try {
+                reply_id = parseInt(message.split(" ", 1)[0].replace("cue2a_", ""));
+                message = message.replace(`cue2a_${reply_id}`, "");
+            } catch {
+                console.log("Error while parsing reply_id");
+                console.error(e);
+            }
+        }
     }
-    const sentMessage = await tg.telegram.sendMediaGroup(
-      CHAT_ID,
-      mediagroup as
-        | (InputMediaPhoto | InputMediaVideo)[]
-        | InputMediaVideo[]
-        | InputMediaDocument[],
-      { reply_parameters: reply_id ? { message_id: reply_id } : undefined }
-    );
-    await tg.telegram.editMessageCaption(
-      sentMessage[0].chat.id,
-      sentMessage[0].message_id,
-      undefined,
-      `cue2a_${sentMessage[0].message_id}\n${sentMessage[0].caption ?? ""}`
-    );
-    return { ok: true, message: "success" };
-  }
 
-  const sentMessage = await tg.telegram.sendMessage(CHAT_ID, message ?? "", {
-    reply_parameters: reply_id ? { message_id: reply_id } : undefined,
-  });
-  if (sentMessage) {
-    await tg.telegram.editMessageText(
-      CHAT_ID,
-      sentMessage.message_id,
-      undefined,
-      `cue2a_${sentMessage.message_id}\n${sentMessage.text}`
-    );
-    return { ok: true, message: "Отправлено успешно" };
-  }
+    const tg = new Telegraf(BOT_TOKEN);
 
-  return { ok: false, message: "Что-то пошло не так." };
+    if (files[0].size > 0) {
+        const mediagroup = [];
+
+        const allImagesOrVideos = files.every((file) => file.type.startsWith("image/") || file.type.startsWith("video/"));
+        const allAudios = files.every((file) => file.type.startsWith("audio/"));
+
+        if (allImagesOrVideos) {
+            for (const file of files) {
+                mediagroup.push({
+                    type: file.type.split("/")[0] == "image" ? "photo" : "video", media: {
+                        source: Buffer.from(await file.arrayBuffer()), filename: file.name,
+                    }, caption: message,
+                });
+            }
+        } else if (allAudios) {
+            for (const file of files) {
+                mediagroup.push({
+                    type: file.type.split("/")[0], media: {
+                        source: Buffer.from(await file.arrayBuffer()), filename: file.name,
+                    }, caption: message,
+                });
+            }
+        } else {
+            for (const file of files) {
+                mediagroup.push({
+                    type: "document", media: {
+                        source: Buffer.from(await file.arrayBuffer()), filename: file.name,
+                    }, caption: message,
+                });
+            }
+        }
+        try {
+            const sentMessage = await tg.telegram.sendMediaGroup(CHAT_ID, mediagroup as | (InputMediaPhoto | InputMediaVideo)[] | InputMediaVideo[] | InputMediaDocument[], {reply_parameters: reply_id ? {message_id: reply_id} : undefined});
+            await tg.telegram.editMessageCaption(sentMessage[0].chat.id, sentMessage[0].message_id, undefined, `cue2a_${sentMessage[0].message_id}\n${sentMessage[0].caption ?? ""}`);
+            return {ok: true, message: "success"};
+        } catch {
+            return {ok: false, message: "Ошибка при отпрвке сообщения"}
+        }
+
+    }
+
+    const sentMessage = await tg.telegram.sendMessage(CHAT_ID, message ?? "", {
+        reply_parameters: reply_id ? {message_id: reply_id} : undefined,
+    });
+    if (sentMessage) {
+        await tg.telegram.editMessageText(CHAT_ID, sentMessage.message_id, undefined, `cue2a_${sentMessage.message_id}\n${sentMessage.text}`);
+        return {ok: true, message: "Отправлено успешно"};
+    }
+
+    return {ok: false, message: "Что-то пошло не так."};
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,54 @@
 "use client"
-import { sendMessageToTelegram } from "./actions";
-import { Button } from "@/components/ui/button";
-import { Textarea } from "@/components/ui/textarea";
-import { Card, CardContent } from "@/components/ui/card";
-import { useActionState } from "react";
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
-import { Input } from "@/components/ui/input";
+import {sendMessageToTelegram} from "./actions";
+import {Button} from "@/components/ui/button";
+import {Textarea} from "@/components/ui/textarea";
+import {Card, CardContent} from "@/components/ui/card";
+import {useActionState} from "react";
+import {Alert, AlertDescription, AlertTitle} from "@/components/ui/alert";
+import {Input} from "@/components/ui/input";
+import Script from "next/script";
 
 export default function MessageForm() {
-  const [state, action, pending] = useActionState(sendMessageToTelegram, undefined)
+    const sendMessageToTelegramWithCaptcha = async (_: unknown, form: FormData) => {
+        try {
+            const token = await (window as unknown).grecaptcha.execute(process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!, {action: 'submit_form'});
+            return await sendMessageToTelegram(_, form, token)
 
-  return (
-    <>
-      <div className="w-screen h-screen overflow-hidden flex justify-center items-center">
-        <Card className="w-full max-w-md mx-auto p-4">
-          <CardContent>
-            <form action={action} className="space-y-4">
-              {state !== undefined &&
-                (
-                  <Alert variant={state.ok ? "default" : "destructive"}>
-                    <AlertTitle>
-                      Сообщение {state.ok ? "отправлено успешно" : "не отправлено"}
-                    </AlertTitle>
-                    <AlertDescription>
-                      {state.message}
-                    </AlertDescription>
-                  </Alert>
-                )}
-              <Textarea
-                placeholder="Your message"
-                name="message"
-              />
-              <Input type="file" name="files" multiple />
-              <Button disabled={pending} type="submit" className="w-full">
-                Send Message
-              </Button>
-            </form>
-          </CardContent>
-        </Card>
-      </div>
-    </>
+        } catch (e) {
+            console.error(e);
+            return {ok: false, message: "Ошибка при работе с капчей"}
+        }
+    }
+    const [state, action, pending] = useActionState(sendMessageToTelegramWithCaptcha, undefined)
 
-  );
+    return (<>
+            <Script strategy="beforeInteractive"
+                    src="https://www.google.com/recaptcha/api.js?render=6LdSmQMrAAAAAIr67-DJkwpwLAZJ2sd8ajk9y7o3"></Script>
+            <div className="w-screen h-screen overflow-hidden flex justify-center items-center">
+                <Card className="w-full max-w-md mx-auto p-4">
+                    <CardContent>
+                        <form action={action} className="space-y-4">
+                            {state !== undefined && (<Alert variant={state.ok ? "default" : "destructive"}>
+                                <AlertTitle>
+                                    Сообщение {state.ok ? "отправлено успешно" : "не отправлено"}
+                                </AlertTitle>
+                                <AlertDescription>
+                                    {state.message}
+                                </AlertDescription>
+                            </Alert>)}
+                            <Textarea
+                                placeholder="Your message"
+                                name="message"
+                            />
+                            <Input type="file" name="files" multiple/>
+                            <Button disabled={pending} type="submit" className="w-full">
+                                Send Message
+                            </Button>
+                        </form>
+                    </CardContent>
+                </Card>
+            </div>
+        </>
+
+    );
 }
